e368fcbfb0e07ad71076cc2b087f0a6f8ac338f3
ramBoson <ramamugesh95@gmail.com>
2021-03-18 13:26:12 +0530

    cmd in all appjs file

--- src/App.js
+++ src/App.js
@@ -17,92 +17,15 @@ function App() {
 
         <center>
 
+
+
+          <h1>Hello</h1>
+
           <br></br>
           <br></br>
           <br></br>
-        <Router history={history}>
-          <Switch>
-            <Route path="/" exact>
-              <div class="display-4 mb-1">Choose a route to go to page</div>
-              <br></br><br></br><br></br>
-              
-              <button
-                class="btn btn-info btn-block"
-                type="button"
-                onClick={() => {
-                  history.push("/Nft");
-                }}
-              >
-                Nft Deploy Contract     
-              </button>
-
-
-
-
-              <button
-                class="btn btn-info btn-block"
-                type="button"
-                onClick={() => {
-                  history.push("/Newpage");
-                }}
-              >
-                Get Single Image Page
-              </button>
-
-              <button
-                class="btn btn-info btn-block"
-                type="button"
-                onClick={() => {
-                  history.push("/Sendpage");
-                }}
->
-  Transfer NFT-Owner
-              </button>
-
-              <button
-                class="btn btn-info btn-block"
-                type="button"
-                onClick={() => {
-                  history.push("/Tokencreate");
-                }}
-              >
-                Mint-Token
-              </button>
-
-
-              <button
-                class="btn btn-info btn-block"
-                type="button"
-                onClick={() => {
-                  history.push("/Printallimage");
-                }}
-              >
-                PrintallImage Page
-              </button>
-
-
-            </Route>
-            
-            <Route path="/Nft">
-              <Nf />
-            </Route>
-            <Route path="/Newpage">
-              <NewPage />
-            </Route>
-            <Route path="/Sendpage">
-              <Sendpage />
-            </Route>
-            <Route path="/Tokencreate">
-              <Tokencreate />
-            </Route>
-            <Route path="/Printallimage">
-              <Printallimage />
-            </Route>
-          </Switch>
-        </Router>
-        </center>
-      </div>
-      
+          </center></div>
+        
     </div>
   );
 }
